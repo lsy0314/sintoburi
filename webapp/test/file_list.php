f85c10981c0d57423fe8302285c46b32b7dff1fb
lsy0314 <34805004+lsy0314@users.noreply.github.com>
2018-07-11 23:34:43 +0900

    Update file_list.php

--- webapp/test/file_list.php
+++ webapp/test/file_list.php
@@ -1,39 +1,84 @@
 
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>업로드 파일 목록</title>
-</head>
-<body>
-<h3>업로드 파일 목록</h3>
-<table border="1">
-<tr>
-	<th>파일 아이디</th>
-	<th>원래 파일명</th>
-	<th>저장된 파일명</th>
-</tr>
-<?php
-$db_conn = mysqli_connect("localhost", "testdbadm", "testdbadm", "testdb");
-$query = "SELECT file_id, name_orig, name_save FROM upload_file ORDER BY reg_time DESC";
-$stmt = mysqli_prepare($db_conn, $query);
-$exec = mysqli_stmt_execute($stmt);
-$result = mysqli_stmt_get_result($stmt);
-while($row = mysqli_fetch_assoc($result)) {
-?>
-<tr>
-  <td><?= $row['file_id'] ?></td>
-  <td><a href="download.php?file_id=<?= $row['file_id'] ?>" target="_blank"><?= $row['name_orig'] ?></a></td>
-  <td><?= $row['name_save'] ?></td>
-</tr>
-<?php
-} 
+<script type="text/javascript">
 
-mysqli_free_result($result); 
-mysqli_stmt_close($stmt);
-mysqli_close($db_conn);
-?>
-</table>
-<a href="upload.php">업로드 페이지</a>
-</body>
-</html>
+function formSubmit(f) {
+
+    // 업로드 할 수 있는 파일 확장자를 제한합니다.
+
+	var extArray = new Array('hwp','xls','doc','xlsx','docx','pdf','jpg','gif','png','txt','ppt','pptx');
+
+	var path = document.getElementById("upfile").value;
+
+	if(path == "") {
+
+		alert("파일을 선택해 주세요.");
+
+		return false;
+
+	}
+
+	
+
+	var pos = path.indexOf(".");
+
+	if(pos < 0) {
+
+		alert("확장자가 없는파일 입니다.");
+
+		return false;
+
+	}
+
+	
+
+	var ext = path.slice(path.indexOf(".") + 1).toLowerCase();
+
+	var checkExt = false;
+
+	for(var i = 0; i < extArray.length; i++) {
+
+		if(ext == extArray[i]) {
+
+			checkExt = true;
+
+			break;
+
+		}
+
+	}
+
+
+
+	if(checkExt == false) {
+
+		alert("업로드 할 수 없는 파일 확장자 입니다.");
+
+	    return false;
+
+	}
+
+	
+
+	return true;
+
+}
+
+</script>
+
+
+
+<form name="uploadForm" id="uploadForm" method="post" action="upload_process.php" 
+
+      enctype="multipart/form-data" onsubmit="return formSubmit(this);">
+
+    <div>
+
+        <label for="upfile">첨부파일</label>
+
+        <input type="file" name="upfile" id="upfile" />
+
+    </div>
+
+    <input type="submit" value="업로드" />
+
+</form>
