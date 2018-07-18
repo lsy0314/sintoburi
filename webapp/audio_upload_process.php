6f3209523022301f88c24e98c556c04a54df89a6
HyunJun Lim <hjoon0510@gmail.com>
2018-07-18 23:36:16 +0900

    Added how to control max number of audio file (5)
    I added how to control max number of audio file (5)
    
    Signed-off-by: HyunJun Lim <hjoon0510@gmail.com>

--- webapp/audio_upload_process.php
+++ webapp/audio_upload_process.php
@@ -14,6 +14,40 @@
 //
 
 include('webapp_config.php');
+
+// change last character to 0.
+// For example, convert 201805051635 to 201805051630
+date_default_timezone_set("Asia/Seoul");
+$t=time();
+$curr_time = date("YmdHi",$t);
+$curr_time = substr_replace($curr_time, "", -1)."0";
+
+// ----------------------calculate the number of audio data
+
+// connect to mysql database
+$db_conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+$count = 0; 
+// https://dev.mysql.com/doc/refman/8.0/en/pattern-matching.html
+// Pattern Matching: Use the LIKE or NOT LIKE comparison operators 
+$query = "SELECT file_id, name_orig, name_save, reg_time, store_name, audio_msg FROM upload_file WHERE name_save LIKE '".$curr_time."%' ORDER BY reg_time DESC";
+$stmt = mysqli_prepare($db_conn, $query);
+$exec = mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+while($row = mysqli_fetch_assoc($result)) {
+    $count= $count+1;
+} 
+mysqli_free_result($result); 
+mysqli_stmt_close($stmt);
+mysqli_close($db_conn);
+//echo "current time: $curr_time , audio data count: $count . <br>";
+//die("just test.");
+
+
+// if the number of audio data exceeds 5, stop program.
+if ($count > 1)
+    die("Unable to upload audio file because seller can upload audio files until 5. <br> <a href=./audio_file_list.php>오디오 파일 리스트로 이동하기</a>");
+ 
+// ------------------------- upload audio file to mysql database and audio folder
 $db_conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 if(isset($_FILES['upfile']) && $_FILES['upfile']['name'] != "") {
@@ -39,12 +73,6 @@ if(isset($_FILES['upfile']) && $_FILES['upfile']['name'] != "") {
    $audio_msg = $_POST['message']; 
 
  
-   // change last character to 0.
-   // For example, convert 201805051635 to 201805051630
-    date_default_timezone_set("Asia/Seoul");
-    $t=time();
-    $curr_time = date("YmdHi",$t);
-    $curr_time = substr_replace($curr_time, "", -1)."0";
     
     $path = $curr_time . "_" . md5(microtime()) . '.' . $ext;
 
