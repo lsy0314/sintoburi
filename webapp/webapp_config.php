ac6638d0e36c287e73d87a828a72d35b5ee02fe4
HyunJun Lim <hjoon0510@gmail.com>
2018-07-22 11:04:38 +0900

    Updated configuration file
    I updated configuration file for max audio numbers.
    
    Signed-off-by: HyunJun Lim <hjoon0510@gmail.com>

--- webapp/webapp_config.php
+++ webapp/webapp_config.php
@@ -1,6 +1,11 @@
 <?php
+// database information
 $db_host = 'localhost';
 $db_name = 'sbdb';
 $db_user = 'root';
 $db_pass = 'ggghhh03';
+
+// if the number of audio data exceeds 5, stop program.
+$max_audio_file = 5;
+
 ?>
