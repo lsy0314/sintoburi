8094218437cb54cc670f9b5a14efe48e15075853
HyunJun Lim <hjoon0510@gmail.com>
2018-08-04 16:27:37 +0900

    Modified design of menu.php
    I modified the design of menu.php
    
    Signed-off-by: HyunJun Lim <hjoon0510@gmail.com>

--- webapp/menu.php
+++ webapp/menu.php
@@ -1,7 +1,9 @@
 
 <font size=5 color=blue>
 <img src=../images/carrot.png width=30 height=30></img>
-<a href=./audio_file_list.php>신토불이: 음성파일 관리 리스트 화면</a></font><br>
+<a href=./audio_file_list.php>신토불이: 음성파일 관리 리스트 화면</a></font>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
 <img src=../images/carrot.png width=30 height=30></img>
 <font size=5 color=blue><a href=./event_file_list.php>신토불이: 상점행사 관리 리스트 화면</a></font><br>
