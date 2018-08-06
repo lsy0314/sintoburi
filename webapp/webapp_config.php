925d83c3b0c02526fdeb7914d69c7283eba77bf7
HyunJun Lim <hjoon0510@gmail.com>
2018-08-07 00:32:57 +0900

    Updated config file
    I updated config file.
    
    Signed-off-by: HyunJun Lim <hjoon0510@gmail.com>

--- webapp/webapp_config.php
+++ webapp/webapp_config.php
@@ -5,6 +5,11 @@ $db_name = 'sbdb';
 $db_user = 'root';
 $db_pass = 'ggghhh03';
 
+
+// table information
+$table_name_audio = 'audio_table';
+$table_name_event = 'event_table';
+
 // if the number of audio data exceeds 5, stop program.
 $max_audio_file = 5;
 
