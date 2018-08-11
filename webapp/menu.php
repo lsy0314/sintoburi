1dc09fb1a0fa98e12ad6c04fe28a15105c7316b4
Suyoun Lim <lsy0314@gmail.com>
2018-08-11 14:38:21 +0900

    Modefied menu design
    I modefied menu design.
    Signed-off-by: Suyoun Lim <lsy0314@gmail.com>

--- webapp/menu.php
+++ webapp/menu.php
@@ -1,19 +1,19 @@
 
 <font size=3 color=blue>
 <img src=../images/carrot.png width=30 height=30></img>
-<a href=./user_login.html>회원관리</a></font>
+<a href=./user_login.html>회원 관리</a></font>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
 <font size=3 color=blue>
 <img src=../images/carrot.png width=30 height=30></img>
-<a href=./audio_file_list.php>음성파일 관리 화면</a></font>
+<a href=./audio_file_list.php>상점 음성파일 관리</a></font>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
 <img src=../images/carrot.png width=30 height=30></img>
-<font size=3 color=blue><a href=./event_file_list.php>상점행사 관리 화면</a></font>
+<font size=3 color=blue><a href=./event_file_list.php>상점 이벤트 관리</a></font>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
 <img src=../images/carrot.png width=30 height=30></img>
-<font size=3 color=blue><a href=./event_announce.php>상점행사 공지 화면</a></font><br>
+<font size=3 color=blue><a href=./event_announce.php>상점 이벤트 알림</a></font><br>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
