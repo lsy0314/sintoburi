43b94b432cc08e4da10c6f04e321ae3d7a68078f
HyunJun Lim <hjoon0510@gmail.com>
2018-08-17 00:02:12 +0900

    again update
    I did again update
    
    Signed-off-by: HyunJun Lim <hjoon0510@gmail.com>

---
+++ webapp/body_customer.php
@@ -0,0 +1,16 @@
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<center>
+신토불이 고객화면용 웹페이지<br>
+<br><br>
+<img src=../images/carrot.png width=70 height=70></img>
+</center>
+<br>
--- webapp/menu.php
+++ webapp/menu.php
@@ -18,10 +18,10 @@
 
 <body bgcolor=gray text=#000000 link=#0000cc vlink=#551a8b alink=#ff0000>
 
-<font size=2>Sintoburi 
+<font size=2><b>Sintoburi </b>
 <!--<select size=1 onchange="if(this.value) window.open(this.value);" class="mycolor"> //-->
 <select size=1 onchange="if(this.value) parent.framebody.location=this.value;">
-        <option value="/" selected class=green>[ 상점주인 ]</option>
+        <option value="./body_store.php" selected class=green>[ 상점주인 ]</option>
         <option value="./user_main.php">회원 관리</option>
         <option value="./audio_file_list.php">음성파일 관리</option>
         <option value="./event_file_list.php">이벤트 관리</option>
@@ -30,7 +30,7 @@
 
 <!--<select size=1 onchange="if(this.value) window.open(this.value);" class="mycolor"> //-->
 <select size=1 onchange="if(this.value) parent.framebody.location=this.value;">
-        <option value="/" selected>[  고객용 ]</option>
+        <option value="./body_customer.php" selected>[  고객용 ]</option>
         <option value="./event_announce.php">상점 이벤트 알림</option>
 </select>
 &nbsp;
