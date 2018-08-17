d752660ec159573e7eaf69a376bfde5556a68f16
HyunJun Lim <hjoon0510@gmail.com>
2018-08-17 19:33:52 +0900

    Removed old data files.
    I removed old data files by putting them in ./trash/ folder.
    
    Signed-off-by: HyunJun Lim <hjoon0510@gmail.com>

--- webapp/home.php
+++ webapp/home.php
@@ -12,7 +12,11 @@ require("./menu2.php");
 <br>
 <center>
 신토불이<br>
-<img src=../images/carrot.png width=70 height=70></img>
+<img src=../images/carrot.png width=70 height=70></img><br><br>
+신토불이의 목적은  IoT Software를 이용하여 전통적인 시장의 상점주인들이 시장을 활성화하고<br>
+수익을 더 높일수 있도록 돕는 것입니다. Sintoburi 는 몸과 태어난 땅은 하나라는 것을 의미합니다.<br>
+즉, 제 땅에서 산출된 것이라야 체질에 잘 맞으므로 우리의 전통 음식들을 애용하자라는 뜻을 가지고 있습니다.<br>
+<br><br>
 </center>
 <br>
 </body>
