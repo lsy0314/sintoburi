873504f12868ef4de23b199c19308d178c9a772d
HyunJun Lim <hjoon0510@gmail.com>
2018-08-18 20:41:20 +0900

    updated user loign webapge
    Signed-off-by: HyunJun Lim <hjoon0510@gmail.com>

--- webapp/user_main.php
+++ webapp/user_main.php
@@ -1,3 +1,20 @@
+<style>
+.footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    height: 20%;
+    width: 100%;
+    background-color: gray;
+    color: white;
+    text-align: left;
+}
+</style>
+
+<div class="footer">
+  <p><br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>sintoburi</b></p>
+</div>
+
 <?php
 require("./menu2.php");
 session_start();
@@ -7,12 +24,15 @@ if(!isset($_SESSION['id'])) {
     header('Location: ./user_login.php');
 }
 else{
-    echo "<br><br>";
+    echo "<br>";
+    echo "&nbsp;&nbsp;&nbsp;";
     echo "<b><font color=red>로그인 성공!!!</font></b><br>";
     echo "<br>";
     echo "<li>상점아이디: ".$_SESSION['id']."</li>";
     echo "<li>상점명: ".$_SESSION['name']."</li>";
     echo "<li>비밀번호: (<font color=white>".$_SESSION['password']."</font>)</li>";
+    echo "<br><br>";
+    echo "<img src=./images/login-success.png width=300 border=0></img>";
     echo "<br><br>";
     echo "<a href=user_logout.php>로그아웃</a>";
 }
