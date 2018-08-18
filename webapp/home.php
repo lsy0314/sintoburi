a3a5030d2854b7eadaf5f2af352956a676c83bb4
HyunJun Lim <hjoon0510@gmail.com>
2018-08-18 20:18:09 +0900

    Updated home webapge
    Signed-off-by: HyunJun Lim <hjoon0510@gmail.com>

--- webapp/home.php
+++ webapp/home.php
@@ -2,20 +2,25 @@
 <?php
 require("./menu2.php");
 ?>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+
 <br>
 <br>
 <center>
-신토불이<br>
+<!--
+<img src=./images/home.png width=300 border=0></img>
+//-->
 <img src=../images/carrot.png width=70 height=70></img><br><br>
-신토불이의 목적은  IoT Software를 이용하여 전통적인 시장의 상점주인들이 시장을 활성화하고<br>
-수익을 더 높일수 있도록 돕는 것입니다. Sintoburi 는 몸과 태어난 땅은 하나라는 것을 의미합니다.<br>
-즉, 제 땅에서 산출된 것이라야 체질에 잘 맞으므로 우리의 전통 음식들을 애용하자라는 뜻을 가지고 있습니다.<br>
+<br>
+<font color=#DF5E05 size=4><b>
+"신토불이는 전통시장과 고객들을 이어줍니다."<br>
+</b></font>
+<br><br>
+<table width=90% border=0>
+<tr><td>
+신토불이의 주요 목적은 IoT 소프트웨어를 이용하여 전통적인 시장의 상점주인들이 시장을 활성화하고
+수익을 더 높일수 있도록 돕는 것입니다. 신토불이는 몸과 태어난 땅은 하나라는 것을 의미합니다.
+즉, 제 땅에서 산출된 것이라야 체질에 잘 맞으므로 우리의 전통 음식들을 애용하자라는 뜻을 가지고 있습니다.
+</td></tr></table>
 <br><br>
 </center>
 <br>
