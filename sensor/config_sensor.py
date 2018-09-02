a2eb14252efb79c34c75fbce1de80efbaf43ce57
HyunJun Lim <hjoon0510@gmail.com>
2018-09-02 15:14:23 +0900

    modified file(voice) more good
    I modified file(voice) more good
    
    Signed-off-by: HyunJun Lim <hjoon0510@gmail.com>

--- sensor/config_sensor.py
+++ sensor/config_sensor.py
@@ -4,7 +4,15 @@
 # that contains Python code
 
 #----------- Configuration area -------------------------
+
+# for detector code
 folder = "/var/www/html/sintoburi/sensor/"
 GPIO_PIN = 4
 search_path="../audio/"
 execution_num = 0
+
+# for mysql database code
+host = "localhost"
+user = "root"
+password = "ggghhh03"
+db = "sbdb"
--- sensor/module_play.py
+++ sensor/module_play.py
@@ -2,12 +2,40 @@
 
 # @Author: Hyunjun Lim
 # @Date: Sep-02-2018
-# @Title: module to play audio files and save bell_number value
+# @Title: module to play audio files and save bell_number value into db
 # @License: Star
 
 import os
 import time
 import MySQLdb
+import config_sensor as cfg
+
+def mysql_bell_number(audio_file):
+    # @see https://www.tutorialspoint.com/python/python_database_access.htm
+    # 1. find unique id (name_save field)
+    # 2. save +1 (bell_number field) into database
+
+    # Open database connection
+    db = MySQLdb.connect(cfg.host,cfg.user,cfg.password,cfg.db)
+
+    # Prepare a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Execute the SQL command: Add value 1 to bell_number field.
+    print "[DEBUG] Inserting +1 into bell_number field of database"
+    sql = "UPDATE `audio_table` SET bell_number = bell_number + 1 WHERE `audio_table`.`name_save` = '" + audio_file + "'"
+
+    try:
+       # Execute the SQL command
+       cursor.execute(sql)
+       # Commit your changes in the database
+       db.commit()
+    except:
+       # Rollback in case there is any error
+       db.rollback()
+
+    # disconnect from database server
+    db.close()
 
 # check if an audio file at the current time, let's play audio file
 # the time has to get number between 0 and 9 minutes
@@ -22,34 +50,8 @@ def play_audio_file(audio_file_time, current_time, search_path, audio_file):
         print "[DEBUG] Let's play the audio file:" , cmd
         os.system(cmd)
         
-        # Todo: Add the number of audio play into dtabase
-        # @see https://www.tutorialspoint.com/python/python_database_access.htm
-        # 1. find unique id (name_save field)
-        # 2. save +1 (bell_number field) into database
-        
-        
-        # Open database connection
-        db = MySQLdb.connect("localhost","root","ggghhh03","sbdb")
-        
-        # Prepare a cursor object using cursor() method
-        cursor = db.cursor()
-       
-        # Execute the SQL command: Add value 1 to bell_number field.
-        print "[DEBUG] Inserting +1 into bell_number field of database"
-        sql = "UPDATE `audio_table` SET bell_number = bell_number + 1 WHERE `audio_table`.`name_save` = '" + audio_file + "'"
-        
-        try:
-           # Execute the SQL command
-           cursor.execute(sql)
-           # Commit your changes in the database
-           db.commit()
-        except:
-           # Rollback in case there is any error
-           db.rollback()
-        
-        # disconnect from server
-        db.close()
-
+        #  Add the number of audio play into dtabase
+        mysql_bell_number(audio_file)
 
         # wait for 2 seconds after play audio file.
         print "[DEBUG] Sleeping for specified seconds"
