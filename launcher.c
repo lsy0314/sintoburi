37012a323359a36e6eaf34df137a1240428e6a91
HyunJun Lim <hjoon0510@gmail.com>
2018-09-08 00:49:22 +0900

    updated launcher software
    I updated launcher software for DEBUGING.
    
    Signed-off-by: HyunJun Lim <hjoon0510@gmail.com>

--- launcher.c
+++ launcher.c
@@ -18,9 +18,14 @@ int main(void){
     // Start sintoburi
     printf ("Starting sintoburi Launcher...\n");
     printf ("Press CTRL+C if you want to stop.\n");
+    printf ("\n");
+    printf ("\n");
+    printf ("\n");
+    printf ("\n");
 
     // Step1: run Apache web-server
     // * $ sudo systemctl restart apache2
+    printf("(1/2)[webapp] Starting Apache web-server...\n");
     result = system("sudo systemctl daemon-reload");
     result = system("sudo systemctl restart apache2");
     if (result == -1){
@@ -30,11 +35,12 @@ int main(void){
         printf("[ERROR] apache2: A web-server process could not be executed. return value is %d\n",result); exit(0);
     }
     else
-        printf("(1/2) [webapp] Apache web-server is successfully started.\n");
+        printf("[webapp] Apache web-server is successfully started.\n");
 
     // Step2: run PIR motion sensor
     // * with background: exec /var/www/html/sintoburi/sensor/detector &> /dev/null &
     // * with foreground: /var/www/html/sintoburi/sensor/detector.py 
+    printf("(2/2)[sensor] Starting PIR sensor program...\n");
     result = system("/var/www/html/sintoburi/sensor/detector.py");
     if (result == -1){
         printf("[ERROR] pir sensor: A PIR sensor process could not be created. return value is %d.\n",result); exit(0);
@@ -43,7 +49,7 @@ int main(void){
         printf("[ERROR] pir sensor: A PIR sensor process could not be executed. return value is %d.\n",result); exit(0);
     }
     else
-        printf("(2/2) [sensor] A PIR sensor process is successfully started.\n");
+        printf("[sensor] A PIR sensor process is successfully started.\n");
             
     // display help message to users.
     printf("Please visit http://localhost/ to use web-application.\n");
