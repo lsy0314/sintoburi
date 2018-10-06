1c12b9c13b970f7ff543628fdddf130a7cb3d444
HyunJoon Lim <34376749+hjoon0510@users.noreply.github.com>
2018-10-06 16:10:51 +0900

    Update menu1.php
    I removed unnecessary statements

--- webapp/menu1.php
+++ webapp/menu1.php
@@ -19,7 +19,6 @@
 <body bgcolor=gray text=#000000 link=#0000cc vlink=#551a8b alink=#ff0000>
 
 <font size=2><b>Sintoburi </b>
-<!--<select size=1 onchange="if(this.value) window.open(this.value);" class="mycolor"> //-->
 <select size=1 onchange="if(this.value) parent.framebody.location=this.value;">
         <option value="./body_store.php" selected class=green>[ 상점주인 ]</option>
         <option value="./user_main.php">회원 관리</option>
@@ -28,7 +27,6 @@
 </select>
 &nbsp;
 
-<!--<select size=1 onchange="if(this.value) window.open(this.value);" class="mycolor"> //-->
 <select size=1 onchange="if(this.value) parent.framebody.location=this.value;">
         <option value="./body_customer.php" selected>[  고객용 ]</option>
         <option value="./event_announce.php">상점 이벤트 알림</option>
